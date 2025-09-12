8d32c8107d73c0d49b95375be93bb1076596a31b
papy-pro <jiresselusa127@gmail.com>
2025-09-12 04:35:14 -0700

    add

---
+++ public/js/dashboard.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const menuToggle = document.getElementById('menuToggle');
+  const nav = document.querySelector('.nav');
+
+  menuToggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+  });
+});
